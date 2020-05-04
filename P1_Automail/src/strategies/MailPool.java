37caf9bc9f1fdae490683df9b022584db20ceca0
Iasona1 <62829720+Iasona1@users.noreply.github.com>
2020-05-04 22:13:13 +1000

    almost there

--- P1_Automail/src/strategies/MailPool.java
+++ P1_Automail/src/strategies/MailPool.java
@@ -131,6 +131,7 @@ public class MailPool implements IMailPool {
 						
 						if(added == false) {
 							pool.add(current);
+							System.out.println("ADDED BACK TO POOL");
 							break;
 						}
 						
